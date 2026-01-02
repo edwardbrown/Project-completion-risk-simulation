ea83c04a42eb2302a01461e58b5385a90a9203ac
Edward Brown <edwardbrown@users.noreply.github.com>
2026-01-02 10:57:16 -0500

    Add files via upload
    # This code can be used to simulate project completion failur risk.
    # The first section uses deSolve as an exemplar and provides a chart, though it can be extended.
    # The second section provides the bootsrapping and stores the number of project tasks not completed
    # and calculates the proportion of failures that occur. 
    # Edward G. Brown 2025

---
+++ PCRS.R
@@ -0,0 +1,77 @@
+# This code can be used to simulate project completion failur risk.
+# The first section uses deSolve as an exemplar and provides a chart, though it can be extended.
+# The second section provides the bootsrapping and stores the number of project tasks not completed
+# and calculates the proportion of failures that occur. 
+# Edward G. Brown 2025
+
+library(deSolve)
+
+pcrs_d <- function(t, state, parameters) {
+  with(as.list(c(state)), {
+    db <-  (sample(40:80,1, replace=TRUE) ) # project tasks completed [lower/upper see below]
+    da <-  ifelse ((a - b) < 0, 0, (a - b) ) # tasks left to be completed at end of month
+    list(c(db, da))
+  })
+}
+
+# initial state and control for number and interval of iterations
+state      <- c(b = 40, a = 1000) # b is the initial tasks completed, a is remaining tasks
+times      <- seq(0, 12, by = 1) # 12 us number of months remaining
+
+out <- ode(y = state, times = times, func = pcrs_d, parms = NULL, method="iteration")
+
+matplot.deSolve(out) #generates a plot
+
+# uncomment to display the day the remaining tasks.
+out[,3]
+
+
+# below is the primary function to calculate percentage of time the project task goal is not met
+
+pcrsfunc <- function() {
+  
+  months <- 12 # months left to complete project
+  lower <- 40 # lowest number of tasks observed/expected to be completed
+  upper <- 80 # highest number of tasks observed/expected to be completed
+  
+  threshold <- 0 # goal is to have zero tasks remaining at project end time
+  
+  xbar.completed <- rep(NA,months)
+  xbar.remaining <- rep(NA,months)
+
+  for (i in 1:months)
+    xbar.completed[[i]] <-sample(lower:upper, 1, replace=TRUE)
+  
+  xbar.remaining[[1]] = 1000 # number of tasks remaining at start of simulation
+  
+  for (i in 1:months)
+    xbar.remaining[[i+1]] = ifelse ((xbar.remaining[i] - xbar.completed[i]) <= 0, 0, (xbar.remaining[i] - xbar.completed[i]) ) # remaining tasks
+
+  # alternatively, rather than return tasks remaining you can return success/failure 
+  # over_threshold <- ifelse (xbar.remaining[months] > threshold,1,0) # 1 is failure
+  # return(over_threshold)
+
+    return(xbar.remaining[i])
+} # end of function
+
+
+reps <- 1000 # number of bootstrap replicates
+bresults <- rep(NA,reps) # for storing the bootsrap results
+
+# run the bootsrap
+for (i in 1:reps)
+  bresults[[i]] <- pcrsfunc()
+
+# the following summarize, plot, and provide 95% CI for number of tasks remaining
+sd(bresults)
+summary(bresults)
+hist(bresults)
+quantile(bresults, 0.025)
+quantile(bresults, 0.975)
+
+# store in a data frame
+dataview <- as.data.frame(bresults)
+
+# the following calculates the percentage of failure across all bootstrap replicates
+perc_failure <- length(dataview$bresults[dataview$bresults > 0])/reps
+perc_failure
